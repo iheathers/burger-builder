8fbe8e78b7b738709bb372e244cb65fd347c0103
iheathers <shresthapritam7@gmail.com>
2020-07-22 14:21:49 +0545

    Updates

---
+++ precache-manifest.a744d16cc45c1b67914cbf4a1e341501.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce2bfade5ada7292b3d0",
+    "url": "/burger-builder/static/css/main.046fb99c.chunk.css"
+  },
+  {
+    "revision": "ce2bfade5ada7292b3d0",
+    "url": "/burger-builder/static/js/main.3ead0834.chunk.js"
+  },
+  {
+    "revision": "6168d9880ae57e7f6b00",
+    "url": "/burger-builder/static/js/runtime~main.ad75294f.js"
+  },
+  {
+    "revision": "8baf74fff27d91310f8b",
+    "url": "/burger-builder/static/js/2.a659bb3f.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "7a620f0221e23df7593c10b9b61e9e31",
+    "url": "/burger-builder/index.html"
+  }
+];
